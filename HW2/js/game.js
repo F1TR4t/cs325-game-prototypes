92cb221a208077fd980f626e189026a1e4afcb8b
Mustafa <mfitrat@masonlive.gmu.edu>
2020-02-17 21:17:21 -0500

    Prototyping

--- HW2/js/game.js
+++ HW2/js/game.js
@@ -13,3 +13,36 @@ var config =
 	};
 
 var game = new Phaser.Game(config);
+
+var keys; /// Keyboard Input
+
+var bd; // Sounds to play
+var sn;
+
+var bg; // Temp BG
+
+function preload() {
+	this.load.image('BG', 'assets/bg.jpg');
+
+	this.load.audio('BD', 'assets/bd.wav');
+	this.load.audio('SN', 'assets/sn.wav');
+}
+
+function create() {
+	keys = this.input.keyboard.addKeys("B, H");
+
+	bg = this.add.image(800, 600, 'BG').setOrigin(1);
+
+	bd = this.sound.add('BD');
+	sn = this.sound.add('SN');
+}
+
+function update() {
+	if ( keys.B.isDown ) {
+		bd.play();
+	}
+
+	if ( keys.H.isDown ) {
+		sn.play();
+	}
+}
