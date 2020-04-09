04ec6fcb6bf2a87f87b910f15c0cb0cee83baf51
Big-Lynx-Boy <fitrat.mustafa@gmail.com>
2020-04-08 23:58:43 -0400

    Finished Core Mechanic - Rough Draft

--- HW5/js/Boot.js
+++ HW5/js/Boot.js
@@ -11,39 +11,44 @@ GameStates.makeBoot = function( game ) { // Potentially create global variables 
     
             //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
             game.stage.disableVisibilityChange = true;
-			game.scale.pageAlignHorizontally = true;
+	    game.scale.pageAlignHorizontally = true;
         },
     
         preload: function () { // Preparing assets
     
             // Backgrounds
-			game.load.image('bg_des', 'assets/bg/des.jpg');
-			game.load.image('des_cl1', 'assets/bg/des_cl1.png');
-			game.load.image('des_cl2', 'assets/bg/des_cl2.png');
-			game.load.image('des_cl3', 'assets/bg/des_cl3.png');
+	    game.load.image('bg_des', 'assets/bg/des.jpg');
+	    game.load.image('des_cl1', 'assets/bg/des_cl1.png');
+	    game.load.image('des_cl2', 'assets/bg/des_cl2.png');
+	    game.load.image('des_cl3', 'assets/bg/des_cl3.png');		
 			
-			// Maps
-			game.load.image('road_des', 'assets/maps/des.png');
-			
-			// Cars
-			game.load.image('aston', 'assets/cars/aston.png');
-			game.load.image('audi', 'assets/cars/audi.png');
-			game.load.image('bugatti', 'assets/cars/bugatti.png');
-			game.load.image('corvette', 'assets/cars/corvette.png');
-			game.load.image('dodge', 'assets/cars/dodge.png');
-			game.load.image('ferrari', 'assets/cars/ferrari.png');
-			game.load.image('lambo', 'assets/cars/lamborghini.png');
-			game.load.image('mercedes', 'assets/cars/mercedes.png');
-			game.load.image('porsche', 'assets/cars/porsche.png');
-			game.load.image('supra', 'assets/cars/supra.png');
-			
-			// OST
-			
-			
-			// Other Assets
-			game.load.image('class', 'assets/classic.png');
-			game.load.image('play', 'assets/play.png');
-    
+	    // Maps
+	    game.load.image('road_des', 'assets/maps/des.png');
+	    
+	    // Cars
+	    game.load.image('aston', 'assets/cars/aston.png');
+	    game.load.image('audi', 'assets/cars/audi.png');
+	    game.load.image('bugatti', 'assets/cars/bugatti.png');
+	    game.load.image('corvette', 'assets/cars/corvette.png');
+	    game.load.image('dodge', 'assets/cars/dodge.png');
+	    game.load.image('ferrari', 'assets/cars/ferrari.png');
+	    game.load.image('lambo', 'assets/cars/lamborghini.png');
+	    game.load.image('mercedes', 'assets/cars/mercedes.png');
+	    game.load.image('porsche', 'assets/cars/porsche.png');
+	    game.load.image('supra', 'assets/cars/supra.png');
+
+	    // OST
+
+	    
+	    // Other Assets
+	    game.load.image('class', 'assets/classic.png');
+	    game.load.image('play', 'assets/play.png');
+	    game.load.image('ps', 'assets/ps.png');
+	    game.load.image('pst', 'assets/pstxt.png');
+ 	    game.load.image('gs', 'assets/gs.png');
+	    game.load.image('gst', 'assets/gstxt.png');
+	    game.load.image('ls', 'assets/ls.png');
+	    game.load.image('lst', 'assets/lstxt.png');
         },
     
         create: function () {
--- HW5/js/Game.js
+++ HW5/js/Game.js
@@ -2,47 +2,146 @@
 
 GameStates.makeGame = function( game, shared ) {
     
-    var car1 = null, car2 = null;
-    var road = null;
-    var key1 = null, key2 = null;
-    var music = null;
-    
+    var car1, car2;
+    var road;
+    var key1, key2;
+    var music;
+
+    var currentSpeed1 = 0;
+    var maxVeloc1 = 300;
+
+    var pShift1 = null, gShift1 = null, lShift1 = null;
+    var pShiftTxt, gShiftTxt, lShiftTxt;
+    var gtime1, ptime1, ltime1;
+    var gt1 = 0, pt1 = 0, lt1 = 0, et1 = 1;
+    var timeOff1 = 1;
+
     function quitGame() {
 	// Delete unused objects and show results
+	currentSpeed1 = 0;
+	gt1 = 0, pt1 = 0; lt1 = 0, et1 = 0, timeOff1 = 0;
+	timeOff1 = 0, maxVeloc1 = 0;
+	if ( gShift1 != null ) {
+	    gShift1.destroy();
+	}
+	if ( pShift1 != null ) {
+	    pShift1.destroy();
+	}
+	if ( lShift1 != null ) {
+	    lShift1.destroy();
+	}
+	car1.destroy();
+	key1.destroy();
+	game.time.events.remove(gtime1);
+	game.time.events.remove(ptime1);
+	game.time.events.remove(ltime1);
+	road.destroy();
 
         // Go back to Menu State
         game.state.start('Menu');
 
     }
+
+    function disGoodP1() {
+	et1 = 0;
+	gShift1 = game.add.sprite(520, 200, 'gs');
+	gShift1.fixedToCamera = true;
+	gt1 = 1;
+    }
+
+    function disPerfP1() {
+	gt1 = 0;
+	gShift1.destroy();
+	pShift1 = game.add.sprite(520, 200, 'ps');
+	pShift1.fixedToCamera = true;
+	pt1 = 1;
+    }
+
+    function disLateP1() {
+	pt1 = 0;
+	pShift1.destroy();
+	lShift1 = game.add.sprite(520,200,'ls');
+	lShift1.fixedToCamera = true;
+	lt1 = 1;
+    }
+
+    function resetP1() {
+	if ( gShift1 != null ) {
+	    gShift1.destroy();
+	}
+	if ( pShift1 != null ) {
+	    pShift1.destroy();
+	}
+	if ( lShift1 != null ) {
+	    lShift1.destroy();
+	}
+	game.time.events.remove(gtime1);
+	game.time.events.remove(ptime1);
+	game.time.events.remove(ltime1);
+	et1 = 1, gt1 = 0, pt1 = 0, lt1 = 0;
+	timeOff1 += 2;
+	gtime1 = game.time.events.add(Phaser.Timer.SECOND*(timeOff1+1), disGoodP1, this);
+	ptime1 = game.time.events.add(Phaser.Timer.SECOND*(timeOff1+2), disPerfP1, this);
+	ltime1 = game.time.events.add(Phaser.Timer.SECOND*(timeOff1+4), disLateP1, this);
+    }
     
     return {
     
         create: function () {
+	    // Have Assets Load
+	    gtime1 = game.time.events.add(Phaser.Timer.SECOND*(timeOff1+1), disGoodP1, this);
+	    ptime1 = game.time.events.add(Phaser.Timer.SECOND*(timeOff1+2), disPerfP1, this);
+	    ltime1 = game.time.events.add(Phaser.Timer.SECOND*(timeOff1+4), disLateP1, this);
+
 	    // Have Music Load
 	    
    	    // Have Map Load
-	    road = game.add.tileSprite(0, -5400, 800, 6000, 'road_des');
+	    road = game.add.tileSprite(0, 0, 800, 6000, 'road_des');
 
-	    game.world.setBounds(0, 0, 800, 2000);
+	    game.world.setBounds(0, 0, 800, 6000);
 
 	    // Have Cars Load
-	    car1 = game.add.sprite(390, 440, 'lambo');
-
-	    // Set Cars  up
-	    
+	    car1 = game.add.sprite(game.world.centerX, 5800, 'lambo');
+	    car1.anchor.setTo(0.05, -0.30);
 
 	    // Start Game Physics
 	    game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	    // Enable physics for the cars and set up speeds
-	    
+	    game.physics.enable(car1, Phaser.Physics.ARCADE);
 
+	    // Create Controls
+	    key1 = game.input.keyboard;
+
+	    // Camera follow Car 1 for now
+	    game.camera.follow(car1);
 	    
         },
     
         update: function () {
-    
-            // if Keyboard is pressed down
+	   if ( (et1 == 1) && (gt1 == 0) && (pt1 == 0) && (lt1 == 0) && key1.isDown(Phaser.KeyCode.UP) ) {
+		resetP1();
+		maxVeloc1 += 120;
+	   } else if ( (et1 == 0) && (gt1 == 1) && (pt1 == 0) && (lt1 == 0) && key1.isDown(Phaser.KeyCode.UP) ) {
+		resetP1();
+		maxVeloc1 += 200;
+	   } else if ( (et1 == 0) && (gt1 == 0) && (pt1 == 1) && (lt1 == 0) && key1.isDown(Phaser.KeyCode.UP) ) {
+		resetP1();
+		maxVeloc1 += 250;
+	   } else if ( (et1 == 0) && (gt1 == 0) && (pt1 == 0) && (lt1 == 1) && key1.isDown(Phaser.KeyCode.UP) ) {
+		resetP1();
+		maxVeloc1 += 180;
+	   }
+
+	   if ( currentSpeed1 < maxVeloc1 ) {
+		currentSpeed1++;
+	   }
+	   
+	   game.physics.arcade.velocityFromRotation(game.physics.arcade.angleToXY(car1, 390, -5400), currentSpeed1, car1.body.velocity);
+
+	   if ( car1.body.y <= 0 ) {
+		quitGame();
+	   }
 	}       
     };
 };
