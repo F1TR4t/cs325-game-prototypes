eebd3bf0c65bbdea7f09b93b11ad54e58273464f
Big-Lynx-Boy <fitrat.mustafa@gmail.com>
2020-04-17 18:06:51 -0400

    Menu Aesthetics complete

--- HW6/js/desMenu.js
+++ HW6/js/desMenu.js
@@ -21,12 +21,12 @@ GameStates.makeDesMenu = function( game, shared ) {
 	     fg = game.add.sprite(0, 0, 'fg_des');
 
 	     // Menu Assets
-	     playButton = game.add.button(339, 200, 'play_button', play, null, 'over', 'out', 'down');
+	     playButton = game.add.button(339, 100, 'play_button', play, null, 'over', 'out', 'down');
 	},
 
 	update: function() {
-	    if ( bg.x < -2200 ) {
-		bg.x = 50;
+	    if ( bg.x < -3689 ) {
+		bg.x = 0;
 	    }
 	    bg.x -= 1.5; 
 	}
