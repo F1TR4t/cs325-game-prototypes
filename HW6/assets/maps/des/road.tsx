81898408a1a38202b68e1f9b7f1e2548ae77dcba
Mustafa <mfitrat@masonlive.gmu.edu>
2020-04-18 15:27:59 -0400

    More assets

---
+++ HW6/assets/maps/des/road.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="road" tilewidth="10" tileheight="10" tilecount="400" columns="20">
+ <image source="./road.png" width="200" height="200"/>
+</tileset>
