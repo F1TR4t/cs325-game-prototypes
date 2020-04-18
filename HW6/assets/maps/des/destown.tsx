eda0c320c6f440790f5791d1fa49241845b3e7c8
Mustafa <mfitrat@masonlive.gmu.edu>
2020-04-18 17:21:09 -0400

    New Map

--- HW6/assets/maps/des/destown.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="destown" tilewidth="10" tileheight="10" tilecount="9360" columns="80">
- <image source="./destown.png" width="800" height="1176"/>
-</tileset>
--- HW6/assets/maps/des/signs.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="signs" tilewidth="10" tileheight="10" tilecount="400" columns="20">
- <image source="./signs.png" width="200" height="200"/>
-</tileset>
